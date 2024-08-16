520f2ada920d418b35a12520f3ef72fbf8d58574
itsxdamdam <bakareayomiku@gmail.com>
2024-08-16 17:31:13 +0100

    feat: display cart page

--- src/CartPage.js
+++ src/CartPage.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import Item from './Item';
 
-const CartPage = ({ items }) => (
+const summarizer = (result, item) => {
+  console.log(result)
+  const existingItem = result.find(i => i.id === item.id)
+
+  if(!existingItem) {
+    result.push({
+      ...item,
+      count: 1
+    })
+  } else {
+    existingItem.count++
+  };
+
+  return result;
+}
+
+const CartPage = ({ items }) => {
+  const cart = items.reduce(summarizer, [])
+  return (
     <div className="CartPage">
       <ul>
-        {items.map(item => (
-          <Item key={item.id} item={item} />
+        {cart.map(item => (
+          <Item key={item.id} item={item}>
+            {item.count}
+          </Item>
         ))}
       </ul> 
     </div>
-  );
+  )
+};
 
 export default CartPage
